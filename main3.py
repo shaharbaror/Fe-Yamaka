991ea6a7a36c0fac12288fdb91433d7b8ca792d1
unknown <goldihon@gmail.com>
2024-04-01 16:33:52 +0300

    identified circle 2d

--- main3.py
+++ main3.py
@@ -9,6 +9,7 @@ import datetime
 from typing import List
 
 
+GRAVITY = 9.81
 class Camera:
     def __init__(self, what_to_see):
         self.cam = cv.VideoCapture(what_to_see)
@@ -98,7 +99,7 @@ class Maskinator (Camera):
         return frame, mask
 
     # Get a frame and return all the circles that are in it
-    def locate_circles(self, frame) -> List[List[float]]:
+    def locate_circles(self, frame) -> List[List[float]] or None:
         circles = []
 
         cnts = cv.findContours(frame.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -110,18 +111,29 @@ class Maskinator (Camera):
             supposed_area = radius ** 2 * np.pi * 0.75
 
             if supposed_area < cv.contourArea(c) < supposed_area * 1.5 and radius > 6:
-                circles.append([x, y, datetime.datetime.now()])
+                circles.append([x, y, radius, time.time()])
 
         return circles
 
-    def pinpoint_cirlces(self, circles:  List[Circle], frame):
+
+    def pinpoint_cirlces(self, circles:  List[Circle], frame, color):
         for c in circles:
 
             center = (int(c.x), int(c.y))
             # only proceed if the radius meets a minimum size
             # draw the circle and centroid on the frame,
             # then update the list of tracked points
-            cv.circle(frame, (center), int(c.radius), (0, 255, 255), 2)
+            cv.circle(frame, (center), int(c.radius), color, 2)
+            cv.circle(frame, center, 5, (0, 0, 255), -1)
+
+    def pinpoint_cirlce(self, circles, frame, color):
+        for c in circles:
+
+            center = (int(c[0]), int(c[1]))
+            # only proceed if the radius meets a minimum size
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv.circle(frame, (center), int(c[2]), color, 2)
             cv.circle(frame, center, 5, (0, 0, 255), -1)
 
     # Gets 3 frames with circles detected in them, and identify which of these circles are the same
@@ -138,35 +150,51 @@ class Maskinator (Camera):
         :type circle_in_frame3: List[List[float]]
         :type time_between1: datetime.timedelta
         :type time_between2: datetime.timedelta
-
+d
         :return: List[Circle]
         """
 
         def calc_next_pos(circle1, circle2):
-            x_distance = circle1[0] - circle2[0]
-            y_distance = circle1[1] - circle2[1]
+            x_distance = circle2[0] - circle1[0]
+            y_distance = circle2[1] - circle1[1]
 
             velocity_x = x_distance / time_between1
             velocity_y = y_distance / time_between1
+            print("velx:", velocity_x, ", vely:", velocity_y)
 
             next_circle_x = circle2[0] + velocity_x * time_between2
-            next_circle_y = circle2[1] + velocity_y * time_between2 - 5 * np.power(time_between2)
-            next_circle = [next_circle_x, next_circle_y]
+            next_circle_y = circle2[1] + velocity_y * time_between2 - (GRAVITY * np.power(time_between2,2))/2
+            next_circle = [next_circle_x, next_circle_y, circle2[2]]
 
-            if next_circle in circle_in_frame3:
-                return Circle(x=next_circle_x,
-                              y=next_circle_y,
-                              radius=circle2[2],
-                              velocity_x=velocity_x,
-                              velocity_y=velocity_y,
-                              time_of_record=datetime.datetime.now())
+            # Now I need to compare balls and put them in the right position
+            min_dis = 9999
+            curr_c = None
+            for c in circle_in_frame3:
+                dist = np.sqrt(np.power(next_circle_x - c[0], 2) + np.power(next_circle_y - c[1], 2))
+                if min_dis > dist and dist < 20:
+                    nim_dis = dist
+                    curr_c = c
+
+            if curr_c:
+
+                return Circle(x=curr_c[0],
+                                  y=curr_c[1],
+                                  radius=curr_c[2],
+                                  velocity_x=velocity_x,
+                                  velocity_y=velocity_y,
+                                  time_of_record=time.time())
+
             return None
+
+
 
         returned_circles = []
 
         for i in range(len(circle_in_frame1)):
             for x in range(len(circle_in_frame2)):
-                returned_circles.append(calc_next_pos(circle_in_frame1[i], circle_in_frame2[x]))
+                estimated_pos = calc_next_pos(circle_in_frame1[i], circle_in_frame2[x])
+                if estimated_pos:
+                    returned_circles.append(estimated_pos)
 
         return returned_circles
 
@@ -184,23 +212,53 @@ def main():
     while True:
         frame, mask = cam.get_frame()
         frame_circles = cam.locate_circles(mask)
-        if 0 < done:
-
-            pos1.next = P(frame_circles, datetime.datetime.now())
+        cam.pinpoint_cirlce(frame_circles, frame, (0, 255, 0))
+        print(frame_circles)
+        # add another circle to the list
+        if frame_circles:
+            pos1.next = P(frame_circles, time.time())
             pos1 = pos1.next
-        if done > 4:
+            done += 1
+        if done >= 3:
 
-            circles = circles.next
-            pos2 = circles.next
-            print(circles.circ, pos2.circ, pos1.circ, pos2.t - circles.t, pos1.t - pos2.t)
-            circle = cam.identify_circles(circles.circ, pos2.circ, pos1.circ, pos2.t - circles.t, pos1.t - pos2.t)
-            print(circle)
-            cam.pinpoint_cirlces(circle, frame)
+            circles = circles.next  # position 1
+            pos2 = circles.next  # position 2
+            done -= 1
 
-        done += 1
+            estimated_positions = cam.identify_circles(circle_in_frame1=circles.circ,
+                                                       circle_in_frame2=pos2.circ,
+                                                       circle_in_frame3=pos1.circ,
+                                                       time_between1=pos2.t - circles.t,
+                                                       time_between2=pos1.t - pos2.t)
+
+            if estimated_positions:
+                cam.pinpoint_cirlces(estimated_positions, frame, (0, 255, 255))
+                print("estimated", estimated_positions[0].x, estimated_positions[0].y)
+
+
+
+        # if 0 < done and len(frame_circles) > 0:
+        #
+        #     pos1.next = P(frame_circles, time.time())
+        #     pos1 = pos1.next
+        #
+        # if done > 4:
+        #
+        #     circles = circles.next
+        #     pos2 = circles.next
+        #     print(circles.circ, pos2.circ, pos1.circ, pos2.t - circles.t, pos1.t - pos2.t)
+        #     circle = cam.identify_circles(circles.circ, pos2.circ, pos1.circ, pos2.t - circles.t, pos1.t - pos2.t)
+        #     print(circle)
+        #     cam.pinpoint_cirlce(circles.circ, frame, (255, 255, 0))
+        #     cam.pinpoint_cirlce(pos1.circ, frame, (255, 0, 255))
+        #     cam.pinpoint_cirlce(pos2.circ, frame, (0, 255, 0))
+        #     cam.pinpoint_cirlces(circle, frame, (0, 255, 255))
+
+
         cv.imshow("frame", frame)
+        cv.imshow("mask", mask)
         # if the user pressed "Q" end the program
-        if cv.waitKey(1) & 0xFF == ord("q"):
+        if cv.waitKey(0) & 0xFF == ord("q"):
             break
 
     cv.destroyAllWindows()
